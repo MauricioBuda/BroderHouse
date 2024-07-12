ef1aabdec602957ce7b35e7ae687a72f71c714f9
MauricioBuda <117939414+MauricioBuda@users.noreply.github.com>
2024-07-12 12:42:56 +0000

    configurando emailJS

---
+++ js/app.js
@@ -0,0 +1,43 @@
+import Swal from 'sweetalert2'
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Obtener los datos del formulario
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    const message = document.getElementById('message').value;
+    const mail = document.getElementById('email').value;
+
+    // Crear el objeto del correo electrónico
+    const templateParams = {
+        from_name: name,
+        to_name: 'Mauricio Ariel Buda',
+        phone: phone,
+        message: message,
+        from_email: mail,
+    };
+
+    // Enviar el correo electrónico
+    emailjs.send('service_5y2quzg', 'template_7c5puch', templateParams)
+        .then(function(response) {
+            Swal.fire({
+                position: "center",
+                icon: "success",
+                title: "¡Correo enviado!",
+                text: "Muchas gracias, te responderemos a la brevedad",
+                showConfirmButton: false,
+                timer: 1500
+              });
+        }, function(error) {
+            console.log('FAILED...', error);
+            Swal.fire({
+                position: "center",
+                icon: "error",
+                title: "¡Algo falló!",
+                text: "Por favor, revisá tu conexión y volvé a intentar",
+                showConfirmButton: false,
+                timer: 2500
+              });
+        });
+});
